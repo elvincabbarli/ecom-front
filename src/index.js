55727d3a5a7ebef508c47097d579812bb95fbe7d
elvin <cabbarli.elvin75@gmail.com>
2023-12-31 11:59:16 +0400

    's'

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import CartProvider from './context/CardProvider';
 import { Layout } from './layout/Layout';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <CartProvider>
       <Layout>
           <App />
       </Layout>
     </CartProvider>
-  </BrowserRouter>
+  </HashRouter>
 
 );
 
